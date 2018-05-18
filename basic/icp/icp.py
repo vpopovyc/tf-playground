a08ffe166527d1468ed84a8e644ff5c5bbf662b6
Vladyslav Popovych <mailuatc@gmail.com>
2018-05-18 18:17:18 +0300

    ~ ICP 3d ~ Needs testing and debugging

--- basic/icp/icp.py
+++ basic/icp/icp.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 base_set = np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 0.0], [2.0, 2.0, 0.0], [3.0, 3.0, 0.0], [4.0, 4.0, 0.0], [5.0, 5.0, 0.0], [6.0, 6.0, 0.0], [7.0, 7.0, 0.0], [8.0, 8.0, 0.0], [9.0, 9.0, 0.0], [10.0, 10.0, 0.0]], np.float32)
 
-angle = 45
-theta = angle * np.pi / 180.
+angle = -45
+theta = angle * np.pi / 180.0
 rot_matrix = np.array([
                        [np.cos(theta), -np.sin(theta), 0], 
                        [np.sin(theta),  np.cos(theta), 0],
@@ -12,9 +12,13 @@ rot_matrix = np.array([
                       ])
 
 for point in base_set:
-    point[1] += np.random.randint(4, high=8)
+  point[1] += 4
+  # point[1] += np.random.randint(4, high=8)
 
-rotated_set = np.dot(base_set, rot_matrix)
+rotated_set = np.array(base_set) 
+
+for i, point in enumerate(rotated_set):
+  rotated_set[i] = np.matmul(rot_matrix, point)
 
 def closest_point(point, scene_points):
   min_distance = np.inf
@@ -24,96 +28,119 @@ def closest_point(point, scene_points):
       if min_distance > distance:
           min_distance = distance
           closest_point = scene_point
+
   assert np.isinf(min_distance) == False
   return closest_point
-
-# TODO
-# def plot_closest_points(base_set, rotated_set, closest_points):
-  # for index in range(base_set.shape[0]):
-  #   plt.plot([rotated_set[index, 0], base_set[wIndex, 0]],
-  #            [rotated_set[index, 1], base_set[wIndex, 1]],
-  #            linestyle=":")
 
 def l2_norm(x):
     x = np.sqrt(np.sum(np.square(x)))
 
 def compute_mse(set):
   set = np.sum(set, axis=0)  
-  return np.dot(np.transpose(set), set)
+  return np.sqrt(np.dot(np.transpose(set), set))
 
-def find_rts(scene_points, model_points):
+def find_rts(scene_points, model_points, translation_only):
 
-    # Find centroids of two point sets
-    scene_centroid = np.mean(base_set, axis=0)
-    model_centroid = np.mean(rotated_set, axis=0)
-    # Compute point coords relative to their centroids
-    scene_prime_points = scene_points - scene_centroid
-    model_prime_points = model_points - model_centroid
-    # Compute optimal quternion
-    Sx = scene_prime_points[:, 0]
-    Sy = scene_prime_points[:, 1]
-    Sz = scene_prime_points[:, 2]
-    Mx = model_prime_points[:, 0]
-    My = model_prime_points[:, 1]
-    Mz = model_prime_points[:, 2]
-    sum_xx = np.sum(Mx * Sx)
-    sum_xy = np.sum(Mx * Sy)
-    sum_xz = np.sum(Mx * Sz)
-    sum_yx = np.sum(My * Sx)
-    sum_yy = np.sum(My * Sy)
-    sum_yz = np.sum(My * Sz)
-    sum_zx = np.sum(Mz * Sx)
-    sum_zy = np.sum(Mz * Sy)
-    sum_zz = np.sum(Mz * Sz)
+  # Find centroids of two point sets
+  scene_centroid = np.mean(scene_points, axis=0)
+  model_centroid = np.mean(model_points, axis=0)
+  # Compute point coords relative to their centroids
+  scene_prime_points = scene_points - scene_centroid
+  model_prime_points = model_points - model_centroid
+  # Compute optimal quternion
+  Sx = scene_prime_points[:, 0]
+  Sy = scene_prime_points[:, 1]
+  Sz = scene_prime_points[:, 2]
 
-    N = np.array(
-    [
-     [ sum_xx + sum_yy + sum_zz, sum_yz - sum_zy         , -sum_xz + sum_zx         , sum_xy - sum_yz         ],
-     [-sum_zy + sum_yz         , sum_xx - sum_zz - sum_yy,  sum_xy + sum_yx         , sum_xz + sum_zx         ],
-     [ sum_zx - sum_xz         , sum_yx + sum_xy         ,  sum_yy - sum_zz - sum_xx, sum_yz + sum_zy         ],
-     [-sum_yz + sum_xy         , sum_zx + sum_xz         ,  sum_zy + sum_yz         , sum_zz - sum_yy - sum_xx]
-    ])
+  Mx = model_prime_points[:, 0]
+  My = model_prime_points[:, 1]
+  Mz = model_prime_points[:, 2]
 
-    ei_values, ei_vector = np.linalg.eig(N)
+  sum_xx = np.sum(Mx * Sx)
+  sum_xy = np.sum(Mx * Sy)
+  sum_xz = np.sum(Mx * Sz)
+  sum_yx = np.sum(My * Sx)
+  sum_yy = np.sum(My * Sy)
+  sum_yz = np.sum(My * Sz)
+  sum_zx = np.sum(Mz * Sx)
+  sum_zy = np.sum(Mz * Sy)
+  sum_zz = np.sum(Mz * Sz)
 
-    quat = ei_vector[:, 0]
+  N = np.array(
+  [
+   [ sum_xx + sum_yy + sum_zz, sum_yz - sum_zy         , -sum_xz + sum_zx         , sum_xy - sum_yz         ],
+   [-sum_zy + sum_yz         , sum_xx - sum_zz - sum_yy,  sum_xy + sum_yx         , sum_xz + sum_zx         ],
+   [ sum_zx - sum_xz         , sum_yx + sum_xy         ,  sum_yy - sum_zz - sum_xx, sum_yz + sum_zy         ],
+   [-sum_yz + sum_xy         , sum_zx + sum_xz         ,  sum_zy + sum_yz         , sum_zz - sum_yy - sum_xx]
+  ])
 
-    q = np.array(
-    [
-     [quat[0], -quat[1], -quat[2], -quat[3]],
-     [quat[1],  quat[0], -quat[3],  quat[2]],
-     [quat[2],  quat[3],  quat[0], -quat[1]],
-     [quat[3], -quat[2],  quat[1],  quat[0]]
-    ])
+  ei_values, ei_vector = np.linalg.eig(N)
 
-    q_bar = np.array(
-    [
-     [quat[0],  quat[1], -quat[2], -quat[3]],
-     [quat[1],  quat[0],  quat[3], -quat[2]],
-     [quat[2], -quat[3],  quat[0],  quat[1]],
-     [quat[3],  quat[2], -quat[1],  quat[0]]
-    ])
+  print("EI values\n", ei_values, "\nEI vector\n", ei_vector)
 
-    rotation = np.dot(np.transpose(q_bar), q)[1:4, 1:4]
-    '''
-               Σ||y'||^2 
-    Scale => ( ––––––––– ) ^ -1
-               Σ||p'||^2
-    '''
-    sum_sp = np.sum(np.dot(np.transpose(scene_prime_points), scene_prime_points))
-    sum_mp = np.sum(np.dot(np.transpose(model_prime_points), model_prime_points))
+  quat = ei_vector[:, 0]
 
-    scale = np.sqrt(sum_sp / sum_mp)
-    print("Scale: ", scale)
+  # q = np.array(
+  # [
+  #  [quat[3], -quat[0], -quat[1], -quat[2]],
+  #  [quat[0],  quat[3], -quat[2],  quat[1]],
+  #  [quat[1],  quat[2],  quat[3], -quat[0]],
+  #  [quat[2], -quat[1],  quat[0],  quat[3]]
+  # ])
 
-    translation = scene_centroid - scale * np.matmul(rotation, model_centroid) 
+  # q_bar = np.array(
+  # [
+  #  [quat[3], -quat[0], -quat[1], -quat[2]],
+  #  [quat[0],  quat[3],  quat[2], -quat[1]],
+  #  [quat[1], -quat[2],  quat[3],  quat[0]],
+  #  [quat[2],  quat[1], -quat[0],  quat[3]]
+  # ])
 
-    print(scene_centroid, model_centroid, scene_centroid - model_centroid, translation)
+  q = np.array(
+  [
+   [quat[0], -quat[1], -quat[2], -quat[3]],
+   [quat[1],  quat[0], -quat[3],  quat[2]],
+   [quat[2],  quat[3],  quat[0], -quat[1]],
+   [quat[3], -quat[2],  quat[1],  quat[0]]
+  ])
 
-    return rotation, scale, translation
+  q_bar = np.array(
+  [
+   [quat[0], -quat[1], -quat[2], -quat[3]],
+   [quat[1],  quat[0],  quat[3], -quat[2]],
+   [quat[2], -quat[3],  quat[0],  quat[1]],
+   [quat[3],  quat[2], -quat[1],  quat[0]]
+  ])
+
+  rotation = np.matmul(np.transpose(q_bar), q)[1:4, 1:4]
+
+  for i, point in enumerate(model_prime_points):
+    model_prime_points[i] = np.matmul(rotation, point)
+
+  '''
+             Σ||y'||^2 
+  Scale => ( ––––––––– ) ^ -1
+             Σ||p'||^2
+  '''
+  sum_sp = np.sum(np.dot(np.transpose(scene_prime_points), scene_prime_points))
+  # print("Sum sp", sum_sp)
+  sum_mp = np.sum(np.dot(np.transpose(model_prime_points), model_prime_points))
+  # print("Sum mp", sum_mp)
+
+  scale = np.sqrt(sum_sp / sum_mp)
+
+  if translation_only:
+    print("Scene centroid: ", scene_centroid, "Model centroid: ", model_centroid)
+    translation = scene_centroid - model_centroid
+  else:
+    translation = scene_centroid - np.matmul(rotation, model_centroid)
+
+  print("Translation\n", translation, "\nRotation\n", rotation, "\nScale", scale)
+
+  return rotation, scale, translation
 
 
-def icp_loop(scene_points, model_points):
+def icp_loop(scene_points, model_points, translation_only=False, rotation_only=False):
 
   closest_points = []
 
@@ -121,31 +148,34 @@ def icp_loop(scene_points, model_points):
     # Find closest point in scene_points
     closest_points.append(closest_point(model_point, scene_points))
 
-  rotation, scale, translation = find_rts(closest_points, model_points)
+  rotation, scale, translation = find_rts(closest_points, model_points, translation_only)
 
-  new_model_points = scale * np.transpose(np.matmul(rotation, np.transpose(model_points)))
+  new_model_points = []
 
-  for point in new_model_points:
-    point += translation
+  if rotation_only:
+    for point in model_points:
+      new_model_points.append(np.matmul(rotation, point))
+  elif translation_only:
+    for point in model_points:
+      new_model_points.append(point + translation)
+  else:
+    for point in model_points:
+      new_model_points.append(np.matmul(rotation, point) + translation)
 
-  loss = compute_mse(closest_points - new_model_points)
 
-  print("Loss: ", loss, compute_mse(scene_points - scene_points))
+  new_model_points = np.array(new_model_points)
 
-  return new_model_points
+  loss = compute_mse(scene_points - new_model_points)
 
-def icp(scene_points, model_points):
-  for i in range(1):
-    model_points = icp_loop(scene_points, model_points)
+  return new_model_points, loss
 
-  return model_points
+if __name__ == "__main__":
+  new_rotated_set = icp(base_set, rotated_set)
 
-new_rotated_set = icp(base_set, rotated_set)
-
-plt.plot(base_set[:, 0], base_set[:, 1])
-plt.plot(rotated_set[:, 0], rotated_set[:, 1])
-plt.plot(new_rotated_set[:, 0], new_rotated_set[:, 1], linestyle='dashed')
-# plt.plot(base_centroid[0], base_centroid[1], 'bo')
-# plt.plot(rotated_centroid[0], rotated_centroid[1], 'bo')
-plt.grid(True)
-plt.show()
+  plt.plot(base_set[:, 0], base_set[:, 1])
+  plt.plot(rotated_set[:, 0], rotated_set[:, 1])
+  plt.plot(new_rotated_set[:, 0], new_rotated_set[:, 1], linestyle='dashed')
+  # plt.plot(base_centroid[0], base_centroid[1], 'bo')
+  # plt.plot(rotated_centroid[0], rotated_centroid[1], 'bo')
+  plt.grid(True)
+  plt.show()
--- basic/icp/icp_3d.py
+++ basic/icp/icp_3d.py
@@ -2,26 +2,21 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+import icp
 
 mset = np.array([
-				  [ 0.0,  0.0, 5.0],
 				  [ 5.0,  0.0, 0.0],
 				  [-5.0,  0.0, 0.0],
 				  [ 0.0,  0.0, 5.0],
-				  [ 5.0,  0.0, 0.0],
-				  [-5.0,  0.0, 0.0],
-				  [ 0.0,  0.0, 5.0],
+				  [ 0.0,  0.0, -5.0],
 				  [ 0.0,  5.0, 0.0],
 				  [ 0.0, -5.0, 0.0]
 				], np.float32)
 
-angle = -45
+angle = 45
 theta = angle * np.pi / 180.0
 rot_matrix = np.array([
-                       [np.cos(theta), -np.sin(theta), 0], 
+                       [np.cos(theta), -np.sin(theta), 0],
                        [np.sin(theta),  np.cos(theta), 0],
                        [0            ,  0            , 1]
                       ])
@@ -29,16 +24,93 @@ rot_matrix = np.array([
 sset = np.array(mset)
 
 for i, point in enumerate(sset):
-  sset[i] = np.matmul(rot_matrix, point)
+  sset[i] = np.dot(point, rot_matrix) + np.array([10.0, 0.0, 0.0])
 
-colors = cm.viridis(np.array(mset[:, 2]))
+new_sset = np.array(sset)
 
+mset_centroid = np.mean(mset, axis=0)
+sset_centroid = np.mean(sset, axis=0)
+new_sset_centroid = np.mean(new_sset, axis=0)
 
 '''
-ICP here
+<ICP>
 '''
 
-ax.plot_wireframe(np.array([mset[:, 0]]), np.array([mset[:, 1]]), np.array([mset[:, 2]]), rstride=1, cstride=1, color="green")
-ax.plot_wireframe(np.array([sset[:, 0]]), np.array([sset[:, 1]]), np.array([sset[:, 2]]), rstride=1, cstride=1, color="red")
+def translate_my_shit():
+	global new_sset
+	print("~~~~~~~~~~~~~~~ Translating ~~~~~~~~~~~~~~~~~~~~")
+	new_sset, loss = icp.icp_loop(mset, new_sset, translation_only=True)
+	print("Loss: ", loss)
 
-plt.show()
+def rotate_my_shit():
+	global new_sset
+	print("~~~~~~~~~~~~~~~ Rotating ~~~~~~~~~~~~~~~~~~~~~~~")
+	new_sset, loss = icp.icp_loop(mset, new_sset, rotation_only=True)
+	print("Loss: ", loss)
+
+def do_shit():
+	global new_sset
+	print("~~~~~~~~~~~~~~~ Translating & Rotating ~~~~~~~~~~~~~~~~~~~~~~~")
+	new_sset, loss = icp.icp_loop(mset, new_sset)
+	print("Loss: ", loss)
+
+'''
+</ICP>
+'''
+
+def press(event):
+	if event.key == 'w':
+		quit()
+
+	if event.key == 'r':
+		rotate_my_shit()
+
+	if event.key == 't':
+		translate_my_shit()
+
+	if event.key == 'a':
+		do_shit()
+
+	plt.clf()
+	plt.cla()
+	plt.close()
+	draw_my_shit(True if event.key == 'p' else False)
+
+def draw_my_shit(mode):
+	fig = plt.figure(figsize=(20,10))
+	ax = fig.add_subplot(111, projection='3d')
+
+	colors = cm.viridis(np.array(mset[:, 2]))
+	
+	ax.plot_wireframe(np.array([mset[:, 0]]), np.array([mset[:, 1]]), np.array([mset[:, 2]]), rstride=1, cstride=1, color="green")
+	ax.plot_wireframe(np.array([sset[:, 0]]), np.array([sset[:, 1]]), np.array([sset[:, 2]]), rstride=1, cstride=1, color="red")
+	ax.plot_wireframe(np.array([new_sset[:, 0]]), np.array([new_sset[:, 1]]), np.array([new_sset[:, 2]]), rstride=1, cstride=1, color="blue", linestyle="dashed")
+
+	ax.scatter(mset_centroid[0], mset_centroid[1], mset_centroid[2])
+	ax.scatter(sset_centroid[0], sset_centroid[1], sset_centroid[2])
+	ax.scatter(new_sset_centroid[0], new_sset_centroid[1], new_sset_centroid[2])
+	
+	if mode:
+		closest_points = []
+
+		for model_point in new_sset:
+			closest_points.append(icp.closest_point(model_point, mset))
+
+		closest_points = np.array(closest_points)
+
+		line = []
+
+		for i in range(new_sset.shape[0]):
+			line.append(closest_points[i, :])
+			line.append(new_sset[i, :])
+
+		line = np.array(line)
+
+		for i in range(new_sset.size - 1):
+			ax.plot(line[:, 0], line[:, 1], line[:, 2], color="grey", linestyle=":", linewidth=0.5)
+
+	fig.canvas.mpl_connect('key_press_event', press)
+	
+	plt.show()
+
+draw_my_shit(False)
